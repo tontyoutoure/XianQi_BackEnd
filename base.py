f7ccd382d33c1615c9a9a4dd9580a36b0865c38d
tontyoutoure <tontyoutoure@gmail.com>
2022-02-14 19:48:46 +0800

    Update base.py

--- base.py
+++ base.py
@@ -1,15 +1,19 @@
-piece_niu_black = 0
-piece_niu_red = 1
-piece_ju_black = 2
-piece_ju_red = 3
-piece_ma_black = 4
-piece_ma_red = 5
-piece_xiang_black = 6
-piece_xiang_red = 7
-piece_shi_black = 8
-piece_shi_red = 9
-piece_gou_black = 10
-piece_gou_red = 11
+from enum import Enum
+import random
+
+class Piece(Enum):
+	niu_black = 0
+	niu_red = 1
+	ju_black = 2
+	ju_red = 3
+	ma_black = 4
+	ma_red = 5
+	xiang_black = 6
+	xiang_red = 7
+	shi_black = 8
+	shi_red = 9
+	gou_black = 10
+	gou_red = 11
 
 # Player Private Info
 # 0-11: count of pieces of a type the player holds (0-3)
@@ -37,7 +41,7 @@ piece_gou_red = 11
 # 4: if the second player give ups (0-1)
 # 5: if the third player give ups (0-1)
 # 6: pillar count (1-3)
-# 7: player num that owns the pillars (0-2)
+# 7: player index that owns the pillars (0-2)
 # 8-19: number of each type of pieces the first player plays (0-3)
 # 20-31: number of each type of pieces the second player plays/give ups. (0-3) All 0 if send to player other than the second pos one and the second player give ups.
 # 32-43: number of each type of pieces the third player plays/give ups. (0-3) All 0 if send to player other than the second pos one and the third player give ups.
@@ -47,20 +51,43 @@ piece_gou_red = 11
 # 1: want to give up: (0-1)
 # 2-13: number of each type of pieces the first player plays
 
+class PillarSet:
+	def __init__(self, iFirstPlayer, iPillarCount, ePieceType, iPillarID): # what pieces first player plays
+		self.iPillarID = iPillarID
+		self.iOwner = iFirstPlayer
+		self.iPillarCount = iPillarCount
+		self.iFirstPlayerID = iFirstPlayer
+		self.aPieceTypes = [ePieceType]
+
+class Player:
+	owned_pillar = []
+	def __init__(self, hGame, iPlayerID):
+		self.aCurrentPieceHold = [0,0,0,0,0,0,0,0,0,0,0,0] #piece hold, only visible to player self
+		self.aCurrentPieceGiveUp = [0,0,0,0,0,0,0,0,0,0,0,0]
+		self.hGame = hGame
+		self.iPlayerID = iPlayerID
+
+class Game:
+	players = []
+	def __init__(self, pick_player_idx):
+		#deal piece, decide first player
+		self.players[0] = Player(self, 0)
+		self.players[1] = Player(self, 1)
+		self.players[2] = Player(self, 2)
+		piece_pool = [0,0,0,1,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,11]
 
 # This function updates the game state vector and game history vector with legal plays.
 def XianqiMainEngine(aGameState = None, aGameHistory = None, aPlayerPrivateInfos = None, iPlayerDeciding = None, iUpdateTo = None):
 
-	if aGameState[1] == 0;
-
-	pass
+	if aGameState[1] == 0:
+		pass
 
 def CheckPlayLegal(aGameState = None, aGameHistory = None, aPlayerPrivateInfos = None, iPlayerDeciding = None, iUpdateTo = None):
 # whether the game goes on
 	if aGameState[1] == 0:
 		
 
-	return False
+		return False
 	
 def CalculatePay(aGameHistory = None):
 	pass
